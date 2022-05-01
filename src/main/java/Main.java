b495d49c6bbd03a67df172105dc2cd1445e53190
sunaga hiroyuki <piroyuki@sunaganoMacBook-Air.local>
2022-05-02 02:35:33 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Objects;
+
+public class Main {
+
+    /**
+     * 現在日時を取得.
+     *
+     * @param cal {@link Calendar}
+     * @param zone {@link ZoneId}
+     * @return {@link LocalDateTime}
+     */
+    LocalDateTime getCurrentDateTime(Calendar cal, ZoneId zone) {
+        ZoneId zoneId = Objects.isNull(zone) ? ZoneId.systemDefault() : zone;
+        if (Objects.isNull(cal)) return LocalDateTime.now(zoneId);
+
+        // NOTE Instant: エポック秒を表す
+        return LocalDateTime.ofInstant(cal.toInstant(), zoneId);
+    }
+
+    /**
+     * 日時を指定して取得.
+     *
+     * @param year 年
+     * @param month 月
+     * @param day 日
+     * @param hour 時
+     * @param minute 分
+     * @param second 秒
+     * @return {@link LocalDateTime}
+     */
+    LocalDateTime getDateTime(int year, int month, int day, int hour, int minute, int second) {
+        return LocalDateTime.of(year, month, day, hour, minute, second);
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+
+    @InjectMocks
+    Main target;
+
+    /** pattern */
+    DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSSS");
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() {}
+
+    @Test
+    void getCurrentDateTimeTest() {
+        var actual = target.getCurrentDateTime(null, null);
+        assertThat(actual).isNotNull();
+        System.out.println(actual);
+        System.out.println(actual.format(pattern));
+
+        actual = target.getCurrentDateTime(Calendar.getInstance(), null);
+        assertThat(actual).isNotNull();
+        System.out.println(actual);
+
+        // utc
+        var utc = target.getCurrentDateTime(Calendar.getInstance(), ZoneId.of("UTC"));
+        assertThat(utc).isNotNull();
+        System.out.printf("utc: %s\n", utc);
+
+        // utc to jst
+        var jst = ZonedDateTime.of(utc, ZoneId.of("UTC"))
+                .withZoneSameInstant(ZoneId.of("Asia/Tokyo"));
+        System.out.printf("jst: %s\n", jst.toLocalDateTime());
+    }
+
+    @Test
+    void getDateTimeTest() {
+        var actual = target.getDateTime(2022, 5, 2, 10, 30, 15);
+        assertThat(actual)
+                .isNotNull()
+                .isEqualTo(LocalDateTime.of(2022, 5, 2, 10, 30, 15));
+        System.out.println(actual);
+    }
+}
